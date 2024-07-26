320bf0b919f77cbdf9402f37015b28b14aef6959
AyeshaShamail <shamail.ayesha.02@gmail.com>
2024-07-26 15:02:21 +0530

    Craeting components and adding routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Products from "./components/Product";
+import ProductDetail from "./components/ProductDetail";
+import Cart from "./components/Cart";
+import ErrorPage from "./components/ErrorPage";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/productDetail/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
