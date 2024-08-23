33997a9ae245f35d051c8ba0bba088a6a2d4833c
AyeshaShamail <shamail.ayesha.02@gmail.com>
2024-08-23 20:20:33 +0530

    Search

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import Navbar from "./Navbar";
+// import { useFilterContext } from "../utils/filterContext";
 
 const MainHeader = styled.header`
   height: 10rem;
@@ -14,17 +15,48 @@ const MainHeader = styled.header`
   .logo {
     height: 5rem;
   }
+  .filter-search {
+    width: 50%;
+    input {
+      padding: 0.6rem 1rem;
+      width: 100%;
+      max-width: 750px;
+    }
+  }
 
-  @media (max-width: ${({ theme }) => theme.media.mobile});
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    .filter-search {
+      width: 60%;
+    }
+
+    input {
+      max-width: none;
+    }
+  }
 `;
 
 // NAvlink is used if we don't want to refresh our page
 const Header = () => {
+  // const {
+  //   filters: { text },
+  //   updateFilterValue,
+  // } = useFilterContext();
   return (
     <MainHeader>
       <NavLink to="/">
-        <img src="/logo12.png" alt="Logo" width={100} />
+        <img src="/logo12.png" alt="Logo" width={180} />
       </NavLink>
+      {/* <div className="filter-search">
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            type="text"
+            name="text"
+            placeholder="Search Products"
+            value={text}
+            onChange={updateFilterValue}
+          />
+        </form>
+      </div> */}
       <Navbar />
     </MainHeader>
   );
